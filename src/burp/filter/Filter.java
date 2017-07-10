f7bf25c19d92431cc0f177c3456cb6ea33b69ec0
Corey Arthur <corey-dean06@hotmail.com>
2017-07-10 08:40:43 +0000

    Added basic regex filter functionality

--- src/burp/filter/Filter.java
+++ src/burp/filter/Filter.java
@@ -3,8 +3,10 @@ import burp.LogEntry;
 import burp.LogTableModel;
 
 import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class Filter extends RowFilter<Object, Object> {
     enum LogicalOperation {LT,LE,GT,GE,EQ,NE}
@@ -18,35 +20,61 @@ public class Filter extends RowFilter<Object, Object> {
     @Override
     public boolean include(Entry<? extends Object, ? extends Object> entry) {
         LogTableModel tableModel = (LogTableModel) entry.getModel();
-        if(this.left instanceof LogEntry.columnNamesType){
-//            Object lValue = ((LogEntry) entry).getValueByKey((LogEntry.columnNamesType) this.left);
-            Object lValue = entry.getValue(tableModel.getColumnIndexByName(this.left.toString()));
-            if(this.right instanceof LogEntry.columnNamesType){
-                return lValue == ((LogEntry) entry).getValueByName((String) this.right);
-            }else{
-                try {
-                    return checkValue(lValue, this.operation,
-                            lValue.getClass().getConstructor(this.right.getClass()).newInstance(this.right));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
-                }
-            }
-        }else{
-            if(this.right instanceof LogEntry.columnNamesType){
-//                Object rValue = ((LogEntry) entry).getValueByKey((LogEntry.columnNamesType) this.right);
-                Object rValue = entry.getValue(tableModel.getColumnIndexByName(this.right.toString()));
-                try {
-                    return checkValue(rValue, this.operation,
-                            rValue.getClass().getConstructor(this.left.getClass()).newInstance(this.left));
-                } catch (Exception e) {
-                    return false;
-                }
-            }else{
-                return checkValue(left, this.operation, right);
-            }
+        Object lValue = this.left, rValue = this.right;
+        if(this.left instanceof LogEntry.columnNamesType) {
+            int columnNo = tableModel.getColumnIndexByName(this.left.toString());
+            lValue = entry.getValue(tableModel.getTable().convertColumnIndexToModel(columnNo));
         }
+        if(this.right instanceof LogEntry.columnNamesType) {
+            int columnNo = tableModel.getColumnIndexByName(this.right.toString());
+            rValue = entry.getValue(tableModel.getTable().convertColumnIndexToModel(columnNo));
+        }
+        if(this.left instanceof Pattern){
+            return ((Pattern) lValue).matcher(rValue.toString()).matches();
+        }else if(this.right instanceof Pattern){
+            return ((Pattern) rValue).matcher(lValue.toString()).matches();
+        }
+
+        try {
+            return checkValue(lValue, this.operation, rValue);
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        //return checkValue(lValue, this.operation,
+        //                lValue.getClass().getConstructor(this.right.getClass()).newInstance(this.right));
     }
+//    @Override
+//    public boolean include(Entry<? extends Object, ? extends Object> entry) {
+//        LogTableModel tableModel = (LogTableModel) entry.getModel();
+//        if(this.left instanceof LogEntry.columnNamesType){
+//            int columnNo = tableModel.getColumnIndexByName(this.left.toString());
+//            Object lValue = entry.getValue(tableModel.getTable().convertColumnIndexToModel(columnNo));
+//            if(this.right instanceof LogEntry.columnNamesType){
+//                return lValue == ((LogEntry) entry).getValueByName((String) this.right);
+//            }else{
+//                try {
+//                    return checkValue(lValue, this.operation,
+//                            lValue.getClass().getConstructor(this.right.getClass()).newInstance(this.right));
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    return false;
+//                }
+//            }
+//        }else{
+//            if(this.right instanceof LogEntry.columnNamesType){
+//                Object rValue = entry.getValue(tableModel.getColumnIndexByName(this.right.toString()));
+//                try {
+//                    return checkValue(rValue, this.operation,
+//                            rValue.getClass().getConstructor(this.left.getClass()).newInstance(this.left));
+//                } catch (Exception e) {
+//                    return false;
+//                }
+//            }else{
+//                return checkValue(left, this.operation, right);
+//            }
+//        }
+//    }
 
     protected Filter(Object left, String operation, Object right) throws FilterException {
         LogicalOperation op;
@@ -92,10 +120,20 @@ public class Filter extends RowFilter<Object, Object> {
     }
 
     //TODO implement type parser?
-    private Object parseItem(String item){
+    private Object parseItem(String item) throws FilterException {
         try {
             return LogEntry.columnNamesType.valueOf(item.toUpperCase());
         }catch (IllegalArgumentException e){}
+
+        Matcher regexMatcher = Pattern.compile("\\/(.*)\\/").matcher(item);
+        if(regexMatcher.matches()){
+            try {
+                Pattern regexItem = Pattern.compile(regexMatcher.group(1));
+                return regexItem;
+            }catch (PatternSyntaxException pSException){
+                throw new FilterException("Invalid Regex Pattern");
+            }
+        }
 
         if(Pattern.compile("\\\"(.*)\\\"").matcher(item).matches()){
             return item.substring(1, item.length()-1);
@@ -142,27 +180,35 @@ public class Filter extends RowFilter<Object, Object> {
         throw new FilterException("Could not parse filter");
     }
 
-    private boolean checkValue(Object left, LogicalOperation op, Object right){
+    private boolean checkValue(Object left, LogicalOperation op, Object right) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        if(!(left instanceof String)){
+            right = left.getClass().getConstructor(this.right.getClass()).newInstance(this.right);
+        }else if(!(right instanceof String)){
+            left = right.getClass().getConstructor(this.left.getClass()).newInstance(this.left);
+        }
+
         switch (op){
-            case EQ: return (left instanceof String && right instanceof String
-                    && ((String) left).equalsIgnoreCase((String) right)) || left.equals(right);
-            case NE: return !((left instanceof String && right instanceof String
-                    && ((String) left).equalsIgnoreCase((String) right)) || left.equals(right));
+            case EQ: {
+                return left.equals(right);
+            }
+            case NE: {
+                return !left.equals(right);
+            }
             case LT: {
                 return (left instanceof Comparable) && (right instanceof Comparable)
-                        && ((Comparable) left).compareTo((Comparable) right) < 0;
+                        && ((Comparable) left).compareTo(right) < 0;
             }
             case GT: {
                 return (left instanceof Comparable) && (right instanceof Comparable)
-                        && ((Comparable) left).compareTo((Comparable) right) > 0;
+                        && ((Comparable) left).compareTo(right) > 0;
             }
             case LE: {
                 return (left instanceof Comparable) && (right instanceof Comparable)
-                        && ((Comparable) left).compareTo((Comparable) right) <= 0;
+                        && ((Comparable) left).compareTo(right) <= 0;
             }
             case GE: {
                 return (left instanceof Comparable) && (right instanceof Comparable)
-                        && ((Comparable) left).compareTo((Comparable) right) >= 0;
+                        && ((Comparable) left).compareTo(right) >= 0;
             }
             default:
                 return false;
