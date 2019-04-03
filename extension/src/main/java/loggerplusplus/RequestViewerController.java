43a440f560c6bdff6028bb0b01f73b8a7247bd95
CoreyD97 <corey-dean06@hotmail.com>
2019-04-03 08:58:12 +0100

    Actually add controller class this time...

---
+++ extension/src/main/java/loggerplusplus/RequestViewerController.java
@@ -0,0 +1,69 @@
+package loggerplusplus;
+
+import burp.*;
+
+public class RequestViewerController implements IMessageEditorController {
+
+    private IHttpRequestResponse currentItem;
+    private IMessageEditor requestEditor;
+    private IMessageEditor responseEditor;
+
+    public RequestViewerController(IBurpExtenderCallbacks callbacks, boolean requestEditable, boolean responseEditable){
+        requestEditor = callbacks.createMessageEditor(this, requestEditable);
+        responseEditor = callbacks.createMessageEditor(this, responseEditable);
+    }
+
+    public IHttpRequestResponse getDisplayedEntity() {
+        return this.currentItem;
+    }
+
+    public void setDisplayedEntity(IHttpRequestResponse requestResponse) {
+        if(requestResponse.equals(currentItem)) return;
+
+        this.currentItem = requestResponse;
+        if (requestResponse.getRequest() != null) {
+            requestEditor.setMessage(requestResponse.getRequest(), true);
+        }else{
+            requestEditor.setMessage(new byte[0], false);
+        }
+
+        if (requestResponse.getResponse() != null) {
+            responseEditor.setMessage(requestResponse.getResponse(), false);
+        }else {
+            responseEditor.setMessage(new byte[0], false);
+        }
+    }
+
+    public IMessageEditor getRequestEditor() {
+        return requestEditor;
+    }
+
+    public IMessageEditor getResponseEditor() {
+        return responseEditor;
+    }
+
+    @Override
+    public byte[] getRequest()
+    {
+        if(currentItem == null || currentItem.getRequest() == null)
+            return new byte[0];
+        else
+            return currentItem.getRequest();
+    }
+
+    @Override
+    public byte[] getResponse()
+    {
+        if(currentItem == null || currentItem.getResponse() == null)
+            return new byte[0];
+        else
+            return currentItem.getResponse();
+    }
+
+    @Override
+    public IHttpService getHttpService()
+    {
+        if(currentItem == null) return null;
+        return currentItem.getHttpService();
+    }
+}
