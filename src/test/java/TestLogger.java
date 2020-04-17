d04502cb4efd158010f62b7217c00e86ed32e6d1
Corey-Dean Arthur <corey-dean06@hotmail.com>
2020-04-17 09:03:38 +0100

    Use reflection for test startup script to prevent errors when jar not availale

--- src/test/java/TestLogger.java
+++ src/test/java/TestLogger.java
@@ -1,6 +1,14 @@
+import java.lang.reflect.Method;
+
 public class TestLogger {
     public static void main(String[] args) {
         System.out.println("Loading Logger++ as legacy extension...");
-        burp.StartBurp.main(args);
+
+        try {
+            Method main = Class.forName("burp.StartBurp").getMethod("main", String[].class);
+            main.invoke(null, (Object) args);
+        }catch (Exception e){
+            System.err.println("Cannot start burp.");
+        }
     }
 }
