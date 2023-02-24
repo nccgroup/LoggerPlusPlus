693f5e68f90c7dd74cefcbddee8d2102f911e801
Corey <1339555+CoreyD97@users.noreply.github.com>
2023-02-24 11:47:39 +0000

    Log to console when loaded via classpath

--- src/main/java/com/nccgroup/loggerplusplus/logging/LoggingController.java
+++ src/main/java/com/nccgroup/loggerplusplus/logging/LoggingController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
 
 @Log4j2
 public class LoggingController {
@@ -17,6 +18,12 @@ public class LoggingController {
     public LoggingController(IGsonProvider gsonProvider, MontoyaApi montoyaApi) {
         this.gsonProvider = gsonProvider;
         logLevel = gsonProvider.getGson().fromJson(montoyaApi.persistence().preferences().getString(Globals.PREF_LOG_LEVEL), Level.class);
+        if(montoyaApi.extension().filename() == null){ //Loaded from classpath. Log to console!
+            LoggerContext context = (LoggerContext) LogManager.getContext(false);
+            ConsoleAppender.Builder consoleAppender = new ConsoleAppender.Builder();
+            consoleAppender.setName("ConsoleAppender");
+            context.getRootLogger().addAppender(consoleAppender.build());
+        }
         setLogLevel(logLevel);
     }
 
